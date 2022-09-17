06bd1f1ed10efcf79c4b34c907bac5f1d4ebd99a
alouane04 <ariahi@student.1337.ma>
2022-09-17 22:35:24 +0100

    cmd for test leaks

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 18:41:44 by ariahi            #+#    #+#             */
-/*   Updated: 2022/09/15 08:28:45 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/09/17 22:30:19 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,15 @@
 
 static void	ft_free_int_minishell(t_list **envlst)
 {
-	t_list	*lst;
+	t_list	*tmp;
 
-	if (!envlst)
-		return ;
 	while (*envlst)
 	{
-		lst = (*envlst)->next;
-		if (lst)
-			ft_free_env(lst->content);
-		free(lst);
-		*envlst = lst;
+		tmp = (*envlst)->next;
+		ft_free_env((*envlst)->content);
+		free(*envlst);
+		*envlst = tmp;
 	}
-	*envlst = NULL;
 }
 
 int	main(int ac, char **av, char **env)
@@ -39,7 +35,6 @@ int	main(int ac, char **av, char **env)
 		return (1);
 	exe_set_signal(1);
 	line = readline("minishell> ");
-	//line = "echo $54";
 	while (line)
 	{
 		if (ft_strspn(line, " \t\n") < ft_strlen(line))
@@ -56,6 +51,35 @@ int	main(int ac, char **av, char **env)
 		free(line);
 		line = readline("minishell> ");
 	}
+
+	// TO USE LEAKS CMD REFFRESHED EVERY 0.5S WHILE DEBUGING
+	
+	//while true; do leaks minishell10; sleep 0.5; clear; done
+	
+	// TO USE VALGRIND TO DETECT WHERE THE LEAKS COME FROOM
+
+	//valgrind --leak-check=full ./minishell10
+	
+	// FOR TEST PURPASE WITH LLDB IN VSCODE
+	
+	// line = strdup("pwd >> l.txt");
+	// if (ft_strspn(line, " \t\n") < ft_strlen(line))
+	// 	add_history(line);
+	// p = parse(line);
+	// if (p)
+	// {
+	// 	g_shell.exec = 1;
+	// 	if (exe(p, NULL, NULL))
+	// 		g_shell.exit_s = 1;
+	// 	g_shell.exec = 0;
+	// }
+	// free_tree(&p);
+	// free(line);
+
 	ft_putstr_fd("exit\n", 1);
-	return (ft_free_int_minishell(&g_shell.envlst), g_shell.exit_s);
+	ft_free_int_minishell(&g_shell.envlst);
+	
+	// FOR CMD LEAK
+	//while (1);
+	return (g_shell.exit_s);
 }
