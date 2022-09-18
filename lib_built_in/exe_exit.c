85bca7bf3d004861d63d4328e3a7db6fdc7dd180
alouane04 <ariahi@student.1337.ma>
2022-09-18 16:08:16 +0100

    fix exit function

--- lib_built_in/exe_exit.c
+++ lib_built_in/exe_exit.c
@@ -6,7 +6,7 @@
 /*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 00:29:02 by ariahi            #+#    #+#             */
-/*   Updated: 2022/09/13 09:37:45 by ariahi           ###   ########.fr       */
+/*   Updated: 2022/09/18 11:57:53 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 static int	is_num(char *str)
 {
 	errno = 0;
-	if ((ft_atoll(str) == LONG_MAX || ft_atoll(str) == LONG_MIN) && errno)
+	if ((ft_atoll(str) == LLONG_MAX || ft_atoll(str) == LLONG_MIN) && errno)
 		return (0);
 	if (*str == '-' || *str == '+')
 		str++;
@@ -36,6 +36,6 @@ int	exe_exit(int ac, char **av)
 		return (ft_putstr_fd("minishell: exit: too many arguments\n", 2), 1);
 	if (!is_num(av[1]))
 		return (ft_putstr_fd("minishell: exit: ", 2), ft_putstr_fd(av[1], 2),
-			ft_putstr_fd(": numeric argument required\n", 2), 255);
+			ft_putstr_fd(": numeric argument required\n", 2), exit(255), 255);
 	exit((unsigned char)ft_atoll(av[1]));
 }
